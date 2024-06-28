df66fd4848c70df38df17c070169daac62625590
parlament715 <alexfresh700@gmail.com>
2024-06-28 16:20:54 +0700

    miss

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import sys
 from pathlib import Path
 sys.path.insert(0,str(Path(__file__).resolve().parents[1]))
 from loader import bot, dp, rq
-from app.middelwares import CheckerSubscriptionsOnChannel,CheckerOnCallbackDatae
+from app.middelwares import CheckerSubscriptionsOnChannel,CheckerOnCallbackData
 
 
 
