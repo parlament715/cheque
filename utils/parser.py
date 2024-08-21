6b3ccf6debb69aa99465fc14e6ff6c9ebc53760d
parlament715 <alexfresh700@gmail.com>
2024-08-21 15:22:10 +0700

    added user agent

--- utils/parser.py
+++ utils/parser.py
@@ -26,7 +26,7 @@ class Parse:
     _base_url = "https://yandex.ru/maps/213/moscow/?ll=37.734853%2C55.714044&z=10.07"
     _options = webdriver.ChromeOptions()
     _options.add_argument('log-level=3')
-
+    _options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36")
     _options.add_argument("--disable-blink-features=AutomationControlled")
     _options.add_argument("--headless")
     _options.add_argument("--no-sandbox")
@@ -42,7 +42,14 @@ class Parse:
         cls._browser.get(cls._base_url)
         logger.info("Getting page ...")
         await asyncio.sleep(0.25)
-        search_box=cls._browser.find_element("class name", "input__control")
+        try:
+            search_box=cls._browser.find_element("class name", "input__control")
+        except:
+            logging.warning("Capcha")
+            search_box = None
+        if search_box is None:
+            # await asyncio.sleep(200)
+            return None, address
         address = Regex.format_address(address)
         logger.info(f"After regex : {address}")
         search_box.send_keys(address)
@@ -77,7 +84,8 @@ class Parse:
 
 
 async def main():
-    ic(await Parse.parse_coordinates_and_address('115193,РОССИЯ,77,город федерального значения Москва,муниципальный округ Даниловский вн.тер.г., Лихачёва пр-кт, д. 16,к. 1, помещение 1Н'))
+    while True:
+        ic(await Parse.parse_coordinates_and_address('Москва 4-я Тверская-Ямская 27'))
 
 if __name__ == "__main__":
     from regex import Regex
